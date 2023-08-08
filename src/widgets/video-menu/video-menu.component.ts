f68ff7ce7811dfe32c41b7bd841661ca6f716a67
Dmitriy Mishchenko <ripatrip@gmail.com>
2023-08-08 22:16:09 +0300

    add video slider listeners and video time change in not angular way

--- src/widgets/video-menu/video-menu.component.ts
+++ src/widgets/video-menu/video-menu.component.ts
@@ -1,26 +1,29 @@
 import {
   Component,
+  HostListener,
   Inject,
   Input,
   OnDestroy,
   OnInit,
   inject,
 } from "@angular/core";
+import { AssetVersion } from "@application/models/asset-version";
+import { ASSET_VERSION_TYPE } from "@application/models/asset-version-type";
+import {
+  MESSAGE_ACTION,
+  MessageBus,
+} from "@application/services/message-bus.interface";
 import {
   MESSAGE_BUS_TOKEN,
   PAGE_STATE_SERVICE_TOKEN,
 } from "@application/tokens";
 import { Subject, distinctUntilChanged, map, takeUntil } from "rxjs";
-import { ChangeActiveVersionUseCase } from "./usecases/change-active-version";
-import { ASSET_VERSION_TYPE } from "@application/models/asset-version-type";
-import { AssetVersion } from "@application/models/asset-version";
-import {
-  MESSAGE_ACTION,
-  MessageBus,
-} from "@application/services/message-bus.interface";
 import { ASSET_STATE } from "../workspace/services/media-assets.service";
+import { ChangeActiveVersionUseCase } from "./usecases/change-active-version";
 
 const LOADING_CLASS = "loading";
+const VIDEO_DIGITS_ROUND = 5;
+
 @Component({
   selector: "video-menu",
   templateUrl: "./video-menu.component.html",
@@ -28,7 +31,9 @@ const LOADING_CLASS = "loading";
 })
 export class VideoMenuComponent implements OnInit, OnDestroy {
   @Input() currentVersionId: number | null = null;
+
   private destroyed$ = new Subject<void>();
+  private state: "INITIAL" | "POINTER_DOWN" = "INITIAL";
 
   public videoPlaylist$ = inject(PAGE_STATE_SERVICE_TOKEN).state$.pipe(
     map((state) =>
@@ -57,6 +62,44 @@ export class VideoMenuComponent implements OnInit, OnDestroy {
     this.changeActiveVersionUseCase.execute(versionId);
   }
 
+  public onPointerMove(event: PointerEvent): void {
+    if (isOnSlider(event) && this.state === "POINTER_DOWN") {
+      const { id, clientWidth } = event.target as HTMLDivElement;
+
+      if (this.currentVersionId !== +id) {
+        this.changeActiveVersionUseCase.execute(+id);
+      }
+
+      const slider = getSlider(id);
+      const pos = event.offsetX;
+      if (slider) {
+        slider.style.transform = `translateX(${pos}px)`;
+      }
+      this.updateVideoTime(pos, clientWidth, id);
+    }
+  }
+
+  public onPointerDown(event: PointerEvent): void {
+    if (isOnSlider(event)) {
+      this.state = "POINTER_DOWN";
+      const { id, clientWidth } = event.target as HTMLDivElement;
+      if (this.currentVersionId !== +id) {
+        this.changeActiveVersionUseCase.execute(+id);
+      }
+
+      const slider = getSlider(id);
+      const pos = event.offsetX;
+      if (slider) {
+        slider.style.transform = `translateX(${pos}px)`;
+      }
+      this.updateVideoTime(pos, clientWidth, id);
+    }
+  }
+  @HostListener("document:pointerup", ["$event"])
+  public onPointerUp() {
+    this.state = "INITIAL";
+  }
+
   private listenToAssetStateChange() {
     this.messageBus
       .fromAction(MESSAGE_ACTION.ASSET_STATE_CHANGED)
@@ -71,8 +114,32 @@ export class VideoMenuComponent implements OnInit, OnDestroy {
       });
   }
 
+  private updateVideoTime(pos: number, clientWidth: number, id: string) {
+    const timePercentage = getTimePercentageFromPosition(pos, clientWidth);
+    const video = document.getElementById(
+      `videoAsset_${id}`
+    ) as HTMLVideoElement | null;
+    if (video && video.duration) {
+      const newTime = +(timePercentage * video.duration).toFixed(VIDEO_DIGITS_ROUND);
+      video.currentTime = newTime;
+    }
+  }
+
   ngOnDestroy(): void {
     this.destroyed$.next();
     this.destroyed$.complete();
   }
+}
+
+function getSlider(id: string) {
+  return document.getElementById(`slider_${id}`);
+}
+
+function getTimePercentageFromPosition(pos: number, clientWidth: number):number {
+  return +(pos / clientWidth).toFixed(VIDEO_DIGITS_ROUND);
+}
+
+function isOnSlider(event: PointerEvent): boolean {
+  const divTarget = event.target as HTMLDivElement;
+  return divTarget && !!divTarget.id && divTarget.classList.contains("slider");
 }
